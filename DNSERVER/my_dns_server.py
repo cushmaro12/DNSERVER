6640bff55cc5516b40606b6aefceea59df284486
cushmaro12 <93145562+cushmaro12@users.noreply.github.com>
2025-03-03 13:33:33 +0200

    Update my_dns_server.py

--- DNSERVER/my_dns_server.py
+++ DNSERVER/my_dns_server.py
@@ -48,6 +48,7 @@ def main():
     conn = sqlite3.connect(FULL_PATH)
     cursor = conn.cursor()
     select_query = cursor.execute(f"SELECT * from {DB_NAME}")
+    dic_of_cache = select_query.fetchall()
 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server_socket.bind(ADDRESS)
